c1b217e8f952d81b6ccb1782fcf53f664e1c01ec
Muhammad Asad <muhammada@aiologic.io>
2023-04-15 18:31:29 +0100

    Fixed license

--- src/AIOPivotTable.jsx
+++ src/AIOPivotTable.jsx
@@ -1,5 +1,5 @@
 import PivotTable from "./components/index";
 import { registerLicense } from '@syncfusion/ej2-base';
-registerLicense('ORg4AjUWIQA/Gnt2VFhhQlJBfVtdXmNWfFN0RnNddVp5flFEcDwsT3RfQF5jSn1Sd0NmWX1XdnFRQQ==');
+registerLicense('OORg4AjUWIQA/Gnt2VFhhQlJBfVtdXmNWfFN0RnNddVp5flFEcDwsT3RfQF5jSn1Sd0NmWX1XdnFRQQ==');
 
 export default PivotTable;
